d0799653e1e5d6dc3eafabdeed0322ac60920ac5
kaushal <kaushal.jethva@rapidops.com>
2018-08-06 23:08:59 +0530

    auth controller promise implemented

--- server/modules/apis/auth.controller.js
+++ server/modules/apis/auth.controller.js
@@ -3,80 +3,74 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 class AuthController {
-    constructor(app){
-      app.post('/register',this.register);
-      app.post('/login', this.login);
-    }
-
-  register(req,res){
-    if (!req.body.Email) {
-      res.json({success: false, message: 'You must provide an e-mail'});
-    } else if (!req.body.Password) {
-      res.json({success: false, message: 'You must provide a password'});
-    } else {
-      let user = new Auth({
-        Email: req.body.Email.toLowerCase(),
-        Password: req.body.Password
-      });
-
-      Auth.findOne({Email: user.Email}, function (err, foundUser) {
-        if (err) {
-          throw err;
-        } else if (foundUser) {
-          res.send({success: false, message: " User already Exist."});
-        } else {
-          user.save((err) => {
-            if (err) {
-              if (err) {
-                throw err;
-              } else {
-                res.json({success: false, message: 'Could not save user. Error: ', err});
-              }
-            } else {
-              res.json({success: true, message: 'Account registered!'});
-            }
-          });
-
-        }
-
-      });
-
-    }
+  constructor(app) {
+    app.post('/register', this.register);
+    app.post('/login', this.login);
   }
 
-  login(req,res){
+  register(req, res) {
     if (!req.body.Email) {
-      res.json({success: false, message: 'No email was provided'});
-    } else {
-      if (!req.body.Password) {
-        res.json({success: false, message: 'No password was provided.'});
-      } else {
-        Auth.findOne({Email: req.body.Email.toLowerCase()}, (err, user) => {
-          if (err) {
-            res.json({success: false, message: err});
-          } else {
-            if (!user) {
-              res.json({success: false, message: 'User not found.'});
-            } else {
-              const savedPassword = user.Password;
-              if (req.body.Password !== savedPassword) {
-                res.json({success: false, message: 'Password invalid'});
-              } else {
-                const token = jwt.sign({userId: user._id}, crypto, {expiresIn: '24h'});
-                res.json({
-                  success: true,
-                  message: 'Login Successful!',
-                  token: token,
-                  user: user
-
-                });
-              }
-            }
-          }
-        });
-      }
+      return res.send({success: false, message: 'You must provide an e-mail'});
+    }
+    if (!req.body.Password) {
+      return res.json({success: false, message: 'You must provide a password'});
     }
 
+    let newUser = new Auth({
+      Email: req.body.Email.toLowerCase(),
+      Password: req.body.Password
+    });
+
+    Auth.findOne({Email: newUser.Email})
+      .then((user) => {
+        if (user) {
+          return res.send({success: false, message: " User already Exist."});
+        }
+        return newUser;
+      })
+      .then((user) => {
+        return user.save();
+      })
+      .then(() => {
+        return res.send({success: true, message: 'Account registered!'});
+      })
+      .catch((e) => {
+        res.send({success: false, Error: e, message: "Error while registering user."});
+      })
+  }
+
+  login(req, res) {
+    if (!req.body.Email) {
+      return res.send({success: false, message: 'No email was provided'});
+    }
+    if (!req.body.Password) {
+      return res.send({success: false, message: 'No password was provided.'});
+    }
+    let emailAddress = req.body.Email.toLowerCase();
+    let password = req.body.Password;
+    Auth.findOne({Email: emailAddress})
+      .then((user) => {
+        if (!user) {
+          return res.send({success: false, message: 'User not found.'});
+        }
+        const savedPassword = user.Password;
+        if (password !== savedPassword) {
+          return res.send({success: false, message: 'Password invalid'});
+        }
+        return user;
+      })
+      .then((user) => {
+        const token = jwt.sign({userId: user._id}, crypto, {expiresIn: '24h'});
+        res.send({
+          success: true,
+          message: 'Login Successful!',
+          token: token,
+          user: user
+
+        });
+      }).catch((e) => {
+      res.send({success: false, Error: e, message: "Error while logging user."});
+    })
   }
 }
 
